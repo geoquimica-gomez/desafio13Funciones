61f0ad03b856b5957beb741538d1d324752fc6b4
geoquimica-gomez <geoquimica.gomez@gmail.com>
2024-06-17 17:06:21 -0400

    primeros dos requermientos listos

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,11 @@
+// 1. Transforma esta declaración de función a una función de expresión. El ejercicio debe quedar en un archivo nombrado `1_funcion.js` (1 Punto)
+
+function example(a, b, c){
+return a+b+c
+}
+
+// Función de expresión
+
+const example = function (a, b, c) {
+    return a+b+c
+}
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,9 @@
+//Transforma la siguiente función a una arrow function de una línea. Este ejercicio debe quedar en un archivo llamado `2_arrow.js` (1 Punto)
+
+// Función de expresión
+let suma = function(a, b){
+return a + b
+}
+
+// Función de expresión (arrow function)
+let sumaE = (a, b) => a + b;
